425a1193fca47295910b5738e2e8eacea3eee5d1
Purnendu Kaul <purnendu.kaul@walmart.com>
2019-08-20 15:24:43 -0500

    feat engg complete

---
+++ utils.py
@@ -0,0 +1,40 @@
+from string import ascii_letters
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+def attr08_binning(*, x:float) -> str:
+    if(x == 0):
+        return 'z'
+    elif (x>0) & (x<=15):
+        return '0-15'
+    elif (x>15) & (x<=30):
+        return '15-30'
+    else:
+        return 'gt-30'
+
+
+
+def plot_corr_matrix(*, df: pd.DataFrame):
+
+    sns.set(style="white")
+
+    # Compute the correlation matrix
+    corr = df.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    s = sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    return s
